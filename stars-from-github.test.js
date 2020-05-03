329910fd1591cf13fceae85b13956ab4b274ba95
zmertens <zmertens@asu.edu>
2020-05-03 13:05:35 -0700

    Update test suite with more comprehensive expectations

--- stars-from-github.test.js
+++ stars-from-github.test.js
@@ -1,7 +1,26 @@
+const getStars = require('./get-stars')
 
-// Test get request for real user and non-existent user
-// Test request failure when headers are not set
-
-test('#Test should succeed', async () => {
+test('#Sanity check', async () => {
     expect(6).not.toBe(9)
+})
+
+test('#Test known user with stars is not a black hole', async () => {
+    const userName = 'zmertens'
+    await expect(getStars(userName)).resolves.not.toBeNull()
+})
+
+test('#Test empty user is a black hole', async () => {
+    const userName = ''
+    expect.assertions(1)
+    await expect(getStars(userName)).rejects.toThrow('Not Found')
+})
+
+test('#Expect username is undefined', async () => {
+    const userName = undefined
+    await expect(userName).toBeUndefined()
+})
+
+test('#Match a cat in octocat', async () => {
+    const userName = 'octocat'
+    await expect(userName).toMatch(/cat/)
 })
